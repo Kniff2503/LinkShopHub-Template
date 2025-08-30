d6a78313153c32721aa856baeb89a5f8ce10dd77
Kniff2503 <43741072+Kniff2503@users.noreply.github.com>
2025-08-31 00:09:24 +0200

    Add Health Endpoint

---
+++ src/LinkShopHub.Web/Features/Health/HealthEndpoints.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkShopHub.Web.Features.Health;
+
+public static class HealthEndpoints
+{
+    public static void MapHealth(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/health", () => Results.Ok("alive"));
+    }
+}
