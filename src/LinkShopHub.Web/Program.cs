bb1a3f1d299c031337b3571d135f28650d009476
Kniff2503 <43741072+Kniff2503@users.noreply.github.com>
2025-09-02 12:45:59 +0200

    create development user seed

--- src/LinkShopHub.Web/Program.cs
+++ src/LinkShopHub.Web/Program.cs
@@ -1,5 +1,6 @@
 using LinkShopHub.Infrastructure.Data;
 using LinkShopHub.Web.Components;
+using LinkShopHub.Web.Data;
 using LinkShopHub.Web.Features.Billing;
 using LinkShopHub.Web.Features.Health;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +10,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"), npgsqlOptions =>
+        npgsqlOptions.EnableRetryOnFailure()));
 builder.Services.AddMudServices();
 
 builder.Services.AddScoped<StripeCheckoutService>();
@@ -25,6 +27,13 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await DemoSeed.SeedAsync(db);
 }
 
 app.UseHttpsRedirection();
