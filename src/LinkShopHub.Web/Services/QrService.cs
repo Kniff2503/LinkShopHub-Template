11a67065cd827688f227a5a9eca9c905e87b6c89
Kniff2503 <43741072+Kniff2503@users.noreply.github.com>
2025-09-05 21:50:35 +0200

    feat: QR-code & share button (#23)

---
+++ src/LinkShopHub.Web/Services/QrService.cs
@@ -0,0 +1,15 @@
+﻿using QRCoder;
+
+namespace LinkShopHub.Web.Services;
+
+public static class QrService
+{
+    public static string GenerateDataUri(string url)
+    {
+        using var qrGenerator = new QRCodeGenerator();
+        using var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
+        using var qrCode = new BitmapByteQRCode(qrCodeData);
+        var pngBytes = qrCode.GetGraphic(20);
+        return $"data:image/png;base64,{Convert.ToBase64String(pngBytes)}";
+    }
+}
